329d280d754a23815a5ccf2e36d74f59657fbba8
Raj M <rajsuper806@gmail.com>
2021-12-28 21:03:28 +0530

    Crud completed

---
+++ src/pages/Users/UserForm.jsx
@@ -0,0 +1,155 @@
+import React, { useEffect } from 'react'
+import { Grid, } from '@material-ui/core';
+import Controls from "../../components/controls/Controls";
+import { useForm, Form } from '../../components/useForm';
+//import * as userService from "../../services/userService";
+
+
+// const genderItems = [
+//     { id: 'male', title: 'Male' },
+//     { id: 'female', title: 'Female' },
+//     { id: 'other', title: 'Other' },
+// ]
+
+const initialFValues = {
+    id: 0,
+    firstName: '',
+    lastName: '',
+    email: '',
+    mobile: '',
+    //city: '',
+    //gender: 'male',
+    //departmentId: '',
+    //hireDate: new Date(),
+    //isPermanent: false,
+}
+
+export default function UserForm(props) {
+    const { addOrEdit, recordForEdit } = props
+
+    const validate = (fieldValues = values) => {
+        let temp = { ...errors }
+        if ('firstName' in fieldValues)
+            temp.firstName = fieldValues.firstName ? "" : "This field is required."
+        if ('lastName' in fieldValues)
+            temp.lastName = fieldValues.lastName ? "" : "This field is required."
+        if ('email' in fieldValues)
+            temp.email = (/$^|.+@.+..+/).test(fieldValues.email) ? "" : "Email is not valid."
+        if ('mobile' in fieldValues)
+            temp.mobile = fieldValues.mobile.length < 10 || fieldValues.mobile.length > 10 ? "Minimum 10 numbers required." : ""
+        // if ('departmentId' in fieldValues)
+        //     temp.departmentId = fieldValues.departmentId.length != 0 ? "" : "This field is required."
+        setErrors({
+            ...temp
+        })
+
+        if (fieldValues === values)
+            return Object.values(temp).every(x => x === "")
+    }
+
+    const {
+        values,
+        setValues,
+        errors,
+        setErrors,
+        handleInputChange,
+        resetForm
+    } = useForm(initialFValues, true, validate);
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        if (validate()) {
+            addOrEdit(values, resetForm);
+        }
+    }
+
+    useEffect(() => {
+        if (recordForEdit != null)
+            setValues({
+                ...recordForEdit
+            })
+    }, [recordForEdit])
+
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Grid container>
+                <Grid item xs={12}>
+                    <Controls.Input
+                        name="firstName"
+                        label="First Name"
+                        value={values.firstName}
+                        onChange={handleInputChange}
+                        error={errors.firstName}
+                    />
+                    <Controls.Input
+                        name="lastName"
+                        label="Last Name"
+                        value={values.lastName}
+                        onChange={handleInputChange}
+                        error={errors.lastName}
+                    />
+                    <Controls.Input
+                        name="email"
+                        label="Email"
+                        value={values.email}
+                        onChange={handleInputChange}
+                        error={errors.email}
+                    />
+                    <Controls.Input
+                        name="mobile"
+                        label="Mobile"
+                        value={values.mobile}
+                        onChange={handleInputChange}
+                        error={errors.mobile}
+                    />
+                     {/* <Controls.Select
+                        name="departmentId"
+                        label="Department"
+                        value={values.departmentId}
+                        onChange={handleInputChange}
+                        options={employeeService.getDepartmentCollection()}
+                        error={errors.departmentId}
+                    /> */}
+                      <div>
+                        <Controls.Button
+                            type="submit"
+                            text="Submit" />
+                        <Controls.Button
+                            text="Reset"
+                            color="default"
+                            onClick={resetForm} />
+                    </div>
+                    {/* <Controls.Input
+                        name="city"
+                        label="City"
+                        value={values.city}
+                        onChange={handleInputChange}
+                    /> */}
+
+                </Grid>
+                {/* <Grid item xs={6}>
+                    <Controls.RadioGroup
+                        name="gender"
+                        label="Gender"
+                        value={values.gender}
+                        onChange={handleInputChange}
+                        items={genderItems}
+                    /> 
+                   
+                    <Controls.DatePicker
+                        name="hireDate"
+                        label="Hire Date"
+                        value={values.hireDate}
+                        onChange={handleInputChange}
+                    />
+                    <Controls.Checkbox
+                        name="isPermanent"
+                        label="Permanent Employee"
+                        value={values.isPermanent}
+                        onChange={handleInputChange}
+                    />                   
+                </Grid> */}
+            </Grid>
+        </Form>
+    )
+}
